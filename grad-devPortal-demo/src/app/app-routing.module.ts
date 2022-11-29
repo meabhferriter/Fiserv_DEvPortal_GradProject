e23d01b65c09a423f901b811c908587f9e072748
meabhferriter <meabh.ferriter@fiserv.com>
2022-11-29 14:39:51 +0000

    Login page

--- grad-devPortal-demo/src/app/app-routing.module.ts
+++ grad-devPortal-demo/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
+import { AppLoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: AppLoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'resetPassword', component: ResetPasswordComponent },
+  { path: 'homeDashboard', component: HomeDashboardComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
