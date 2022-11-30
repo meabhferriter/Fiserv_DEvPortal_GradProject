f650114873cd3fcaa3fd8f012767d536cc7ddd00
meabhferriter <meabh.ferriter@fiserv.com>
2022-11-30 11:07:15 +0000

    login page

--- grad-devPortal-demo/src/app/app.module.ts
+++ grad-devPortal-demo/src/app/app.module.ts
@@ -7,27 +7,25 @@ import { AppLoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { HomeDashboardComponent } from './home-dashboard/home-dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, AppLoginComponent, RegisterComponent, ResetPasswordComponent, HomeDashboardComponent],
+  declarations: [
+    AppComponent,
+    AppLoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent,
+    HomeDashboardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatInputModule,
-    MatFormFieldModule,
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
