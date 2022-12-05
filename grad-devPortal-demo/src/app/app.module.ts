84c7d3d06a08e335c675f878209dc9647d16d08c
meabhferriter <meabh.ferriter@fiserv.com>
2022-12-05 08:50:09 +0000

    Update app.module.ts

--- grad-devPortal-demo/src/app/app.module.ts
+++ grad-devPortal-demo/src/app/app.module.ts
@@ -7,14 +7,10 @@ import { AppLoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-=======
 import { NgToastModule } from 'ng-angular-popup';
-
->>>>>>> 0cb8af4cb3dee93ca92aea805c2510697ec3c818
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
@@ -39,14 +35,12 @@ import { UserTypeService } from './services/user-type/user-type.service';
     ReactiveFormsModule,
     HttpClientModule,
     BrowserModule,
-<<<<<<< HEAD
     FormsModule,
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-=======
-    FormsModule,NgToastModule
->>>>>>> 0cb8af4cb3dee93ca92aea805c2510697ec3c818
+    FormsModule,
+    NgToastModule,
   ],
   providers: [UserTypeService],
   bootstrap: [AppComponent],
